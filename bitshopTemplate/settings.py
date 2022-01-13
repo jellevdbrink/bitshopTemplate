b07a8bbd0d065683e729a2dfdc506f18c80d9f5d
Jelle van den Brink <jelle@famvandenbrink.net>
2022-01-13 13:23:23 +0100

    secret key verstopt

--- bitshopTemplate/settings.py
+++ bitshopTemplate/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$!c7iv@uo(*v*g7jbt4%_ey%1d7r$f_ba=+0#=gzcjm=t8=^8p'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
